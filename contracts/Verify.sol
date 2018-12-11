3fd62de0a79c37ccffee53f5aec99afc2ea45671
Zlash65 <zarrar65@gmail.com>
2018-12-11 17:28:06 +0530

    solidity code added to recover address from signed message

---
+++ contracts/Verify.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+
+contract Verifier {
+
+	function recoverAddr(bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) public returns (address) {
+		return ecrecover(msgHash, v, r, s);
+	}
+
+	function isSigned(address _addr, bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) public returns (bool ) {
+		return ecrecover(msgHash, v, r, s) == _addr;
+	}
+
+}
