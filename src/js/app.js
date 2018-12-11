739ac1f46c0b2291191633458de8b7fe7e352b87
Zlash65 <zarrar65@gmail.com>
2018-12-11 17:27:40 +0530

    cleaned up application code

--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,42 @@
 App = {
-  web3Provider: null,
-  contracts: {},
+	web3Provider: null,
+	contracts: {},
+	account: '0x0',
 
-  init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+	init: function () {
+		return await App.initWeb3();
+	},
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+	initWeb3: async function () {
+		// TODO: refactor conditional
+		if (typeof web3 !== 'undefined') {
+			// If a web3 instance is already provided by Meta Mask.
+			App.web3Provider = web3.currentProvider;
+			web3 = new Web3(web3.currentProvider);
+		} else {
+			// Specify default instance if no web3 instance provided
+			App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+			web3 = new Web3(App.web3Provider);
+		}
 
-        petsRow.append(petTemplate.html());
-      }
-    });
+		return App.initContract();
+	},
 
-    return await App.initWeb3();
-  },
+	initContract: function () {
+		$.getJSON("Verify.json", function (verifier) {
+			//Instantiate a new truffle contract from the artifactes
+			App.contracts.Verifier = TruffleContract(verifier);
 
-  initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+			//Coonnect provider to intrect with contract
+			App.contracts.Verifier.setProvider(App.web3Provider);
 
-    return App.initContract();
-  },
-
-  initContract: function() {
-    /*
-     * Replace me...
-     */
-
-    return App.bindEvents();
-  },
-
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
-
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
-
-  handleAdopt: function(event) {
-    event.preventDefault();
-
-    var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
-  }
+		});
+	},
 
 };
 
-$(function() {
-  $(window).load(function() {
-    App.init();
-  });
+$(function () {
+	$(window).load(function () {
+		App.init();
+	});
 });
